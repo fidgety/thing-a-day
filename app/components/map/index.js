962bf616f644f3039a409c33070ec4d258da6f82
Matthew Beasley <matthew.beasley@gmail.com>
2015-08-08 23:25:19 +0100

    find nearest point a bicycle can get to where the user clicks

--- app/components/map/index.js
+++ app/components/map/index.js
@@ -1,6 +1,8 @@
 var React = require('react');
 require('./style.scss');
 
+var mapMethods = require('./mapMethods');
+
 module.exports = React.createClass({
     getInitialState: function () {
         return {
@@ -22,11 +24,22 @@ module.exports = React.createClass({
             map: map
         });
 
+        var that = this;
+
         google.maps.event.addListenerOnce(map, 'tilesloaded', function () {
             google.maps.event.addListener(map, 'dragend', function () {
             });
+
             google.maps.event.addListener(map, 'click', function (e) {
+                mapMethods.snapToRoute(e.latLng, function (latLng) {
+                    new google.maps.Marker({
+                        position: latLng,
+                        map: that.state.map,
+                        title: "Hello World!"
+                    });
+                })
             });
+
             google.maps.event.addListener(map, 'zoom_changed', function () {
             });
         });
@@ -37,4 +50,3 @@ module.exports = React.createClass({
 });
 
 var snazzy1 = [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
-var snazzy2 = [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},{"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}];
---
+++ app/components/map/mapMethods.js
@@ -0,0 +1,23 @@
+var directionsService = new google.maps.DirectionsService();
+
+module.exports = {
+    snapToRoute: function (latLng, callback) {
+        this.getDirections(latLng, latLng, function (route) {
+            callback(route.legs[0].start_location);
+        });
+    },
+    getDirections: function (startLatLng, endLatLng, callback) {
+        var request = {
+            origin: startLatLng,
+            destination: endLatLng,
+            travelMode: google.maps.DirectionsTravelMode.BICYCLING
+        };
+
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                return callback(response.routes[0]);
+            }
+            alert(response, status);
+        });
+    }
+};
