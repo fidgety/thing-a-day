c236a46e091fecae008ba9e55192eb7d619b3fd9
Matthew Beasley <matthew.beasley@gmail.com>
2015-08-18 00:03:24 +0100

    include karma file

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    reporters: ['dots'],
+    files: [
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyBqHMoN6nB_EgKgG0rZ9LjC5dNu2UV34KY&libraries=geometry',
+      'tests.webpack.js'
+    ],
+    preprocessors: {
+      'tests.webpack.js': [ 'webpack', 'sourcemap' ] //preprocess with webpack and our sourcemap loader
+    },
+    port: 9876,
+    colors: true,
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false,
+    webpack: { //kind of a copy of your webpack config
+      devtool: 'inline-source-map', //just do inline source maps instead of the default
+      module: {
+        loaders: [
+          { test: /\.js$/, loader: 'babel-loader' }
+        ]
+      }
+    },
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    }
+
+  })
+};
