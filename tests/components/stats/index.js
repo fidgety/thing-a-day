0877e852a9d2cb8c40308f08f6ba35d0fd34c034
Matthew Beasley <matthew.beasley@gmail.com>
2015-08-26 13:29:07 +0100

    add some more tests

--- tests/components/stats/index.js
+++ tests/components/stats/index.js
@@ -3,6 +3,36 @@ var Reflux = require('reflux');
 var TestUtils = require('react/addons').addons.TestUtils;
 var Stats = require('../../../app/components/stats');
 
+function setUpComponent() {
+    var fakeRouteStore = Reflux.createStore({
+        updateNumber: function () {
+            this.trigger({
+                distance: 92000
+            });
+        }
+    });
+
+    var fakeElevationsStore = Reflux.createStore({
+        updateNumber: function () {
+            this.trigger({
+                ascending: 10,
+                descending: 12.2
+            });
+        }
+    });
+    var StatsFactory = require('inject!../../../app/components/stats');
+    var Stats = StatsFactory({
+        '../../stores/route': fakeRouteStore,
+        '../../stores/elevations': fakeElevationsStore
+    });
+
+    var componentUnderTest = TestUtils.renderIntoDocument(<Stats/>);
+    var dom = TestUtils.findRenderedComponentWithType(componentUnderTest, Stats).getDOMNode();
+
+    fakeRouteStore.updateNumber();
+    fakeElevationsStore.updateNumber();
+    return dom;
+}
 describe('stats component', function () {
     it('should render as zero initially', function () {
         var componentUnderTest = TestUtils.renderIntoDocument(<Stats/>);
@@ -11,28 +41,28 @@ describe('stats component', function () {
         dom.getElementsByTagName('span')[0].innerHTML.should.equal('0');
     });
 
-    it('should update number and convert to km when store is updated', function (done) {
-        var fakeStore = Reflux.createStore({
-            updateNumber: function () {
-                this.trigger({
-                    distance: 92000
-                });
-            }
-        });
-
-        // https://www.npmjs.com/package/proxy-loader
-        var StatsFactory = require('inject!../../../app/components/stats');
-        var Stats = StatsFactory({
-            '../../stores/route': fakeStore
-        });
-
-        var componentUnderTest = TestUtils.renderIntoDocument(<Stats/>);
-        var dom = TestUtils.findRenderedComponentWithType(componentUnderTest, Stats).getDOMNode();
-
-        fakeStore.updateNumber();
+    it('should update overall distance, converting to km when store is updated', function (done) {
+        var dom = setUpComponent();
 
         setTimeout( () => {
             dom.getElementsByTagName('span')[0].innerHTML.should.equal('92.00');
+            done();
+        }, 1000);
+    });
+
+    it('should update overall ascending when store is updated', function (done) {
+        var dom = setUpComponent();
+
+        setTimeout( () => {
+            dom.getElementsByTagName('span')[2].innerHTML.should.equal('10.00');
+            done();
+        }, 1000);
+    });
+
+    it('should update overall descending when store is updated', function (done) {
+        var dom = setUpComponent();
+        setTimeout( () => {
+            dom.getElementsByTagName('span')[4].innerHTML.should.equal('12.20');
             done();
         }, 1000);
     });
