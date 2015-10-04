3ed5ec39cb192da50593a3c0b0067b3dcead63de
Matthew Beasley <matthew.beasley@gmail.com>
2015-10-04 22:03:40 +0100

    add icon

--- app/components/picks/picksMap/index.js
+++ app/components/picks/picksMap/index.js
@@ -29,14 +29,26 @@ module.exports = React.createClass({
     },
     _makeToolTip: function (pick) {
         var tooltipDiv = document.createElement('div');
-        var goHereDiv = document.createElement('div');
+        var addDiv = document.createElement('div');
+        var closeDiv = document.createElement('div');
 
-        goHereDiv.innerHTML = 'go here';
-        goHereDiv.onclick = function () {
+        addDiv.innerHTML = '+';
+        addDiv.className = 'picks-add';
+        addDiv.onclick = function () {
             picksActions.pickSelected(pick);
             return false;
         };
-        tooltipDiv.appendChild(goHereDiv);
+        tooltipDiv.appendChild(addDiv);
+
+        closeDiv.innerHTML = '-';
+        closeDiv.className = 'picks-close';
+        closeDiv.onclick = function () {
+            actions.pickUnhighlighted(pick.name);
+            return false;
+        };
+        tooltipDiv.appendChild(addDiv);
+        tooltipDiv.appendChild(closeDiv);
+
         return tooltipDiv;
     },
     render: function () {
